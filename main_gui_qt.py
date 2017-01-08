cd5e2a40449aec32264293f16e05ee288b3e3601
Saumya Rawat <saumya.rawat25@gmail.com>
2017-01-08 12:00:01 +0530

    Shifted GUI to pyqt

---
+++ main_gui_qt.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+import pupil
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType("SafetyFirst.ui")
+
+class MyApp(QMainWindow):
+    def __init__(self):
+        super(MyApp, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.StartMonitoring)
+
+    def StartMonitoring(self):
+        #price = int(self.ui.price_box.toPlainText())
+        #tax = (self.ui.tax_rate.value())
+        #total_price = price + ((tax / 100) * price)
+        #total_price_string = “The total price with tax is: ” + str(total_price)
+        #self.ui.results_window.setText(total_price_string)
+        self.ui.pushButton.setText("Monitoring!")
+        QApplication.processEvents()
+        pupil.main()
+    
+    def closeEvent(self, event):
+        logger.info("stopping spin")
+        self.stylusProximityControlOff()
+        self.deleteLater() 
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
